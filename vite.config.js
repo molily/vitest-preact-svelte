7359c68219fa8f304c9828391a11478c60e14f5b
Mathias Schaefer (molily) <molily@mailbox.org>
2024-06-13 10:09:21 +0200

    Add missing imports

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
+import { dirname, join } from 'node:path';
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import { svelteTesting } from '@testing-library/svelte/vite';
 import preact from '@preact/preset-vite';
+
+const currentDir = dirname(fileURLToPath(import.meta.url));
 
 // https://vitejs.dev/config/
 export default defineConfig({
