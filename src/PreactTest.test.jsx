e186400338587a5d2ee3743a11d0341f7ee1f493
Mathias Schaefer (molily) <molily@mailbox.org>
2024-06-13 09:16:28 +0200

    Fix test

--- src/PreactTest.test.jsx
+++ src/PreactTest.test.jsx
@@ -3,7 +3,6 @@ import { expect, it } from 'vitest';
 import { PreactTest } from './PreactTest.jsx';
 
 it('renders', () => {
-  const { getByRole } = render(<PreactTest />);
-  const heading = getByRole('heading');
-  expect(heading.textContent).toBe('Hello world from Preact');
+  const { baseElement } = render(<PreactTest />);
+  expect(baseElement.textContent).toBe('Hello world from Preact');
 });
